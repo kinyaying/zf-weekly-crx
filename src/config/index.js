dca4324e20c7527afd39d47d7ab0e69c66ef0e86
kinyaying <kinyaying@gmail.com>
2021-08-26 14:26:17 +0800

    feat: 修改图标

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 // const ip = 'weekly.zoo.team'; // 服务器地址
-const ip = '140.143.128.66'; // 服务器地址
+const ip = '140.143.128.166'; // 服务器地址
 const port = '3030'; // 3030在后端项目中写死，可以到后端项目中修改
 export const BASE_URL = `http://${ip}:${port}`; // 后端请求地址
 export const STATIC_URL = `http://${ip}`; // 前端网站地址
