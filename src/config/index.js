f0484e5f9de41db3445807cd6bb1c52a139c6a31
kinyaying <kinyaying@gmail.com>
2021-08-26 14:01:09 +0800

    first commit

---
+++ src/config/index.js
@@ -0,0 +1,16 @@
+// const ip = 'weekly.zoo.team'; // 服务器地址
+const ip = '140.143.128.66'; // 服务器地址
+const port = '3030'; // 3030在后端项目中写死，可以到后端项目中修改
+export const BASE_URL = `http://${ip}:${port}`; // 后端请求地址
+export const STATIC_URL = `http://${ip}`; // 前端网站地址
+// 小报展示外部博客地址
+export const otherBlogs = [
+];
+
+// 其他链接地址
+export const links = {
+  // 前端文档跳转地址
+  frontDocument: 'https://www.yuque.com/zaotalk/ued/zooteam',
+  // 一键内推跳转地址
+  recommendationLink: 'https://www.yuque.com/zaotalk/ued/zooteam',
+};
